8681ba4c8614b32d00a9bbf9ce453cdffd0b5e86
hideokamoto <hide.okamoto@digitalcube.jp>
2016-05-18 19:33:28 +0900

    rename

---
+++ src/index.js
@@ -0,0 +1,23 @@
+'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CommentList extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+			aaa
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<CommentList />,
+	document.getElementById('amimoto-ninja-comment-list')
+)
