a6b26ad419c566480f701356e26e472acf9a0bcc
hideokamoto <hide.okamoto@digitalcube.jp>
2016-05-18 19:38:27 +0900

    get element attributes
    - domain
    - post_id

--- src/index.js
+++ src/index.js
@@ -17,7 +17,13 @@ class CommentList extends React.Component {
 	}
 }
 
-ReactDOM.render(
-	<CommentList />,
-	document.getElementById('amimoto-ninja-comment-list')
-)
+
+var target = document.getElementById('amimoto-ninja-comment-list');
+if ( target != null ) {
+	var domain  = target.getAttribute('data-domain')
+	var post_id = target.getAttribute('data-post-id')
+	ReactDOM.render(
+		<CommentList post_id={post_id} domain={domain}/>,
+		target
+	)
+}
